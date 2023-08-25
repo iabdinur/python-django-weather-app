d165d2036bd97ebc498c02e476259e70385d5df0
iabdinur <iabdinur1@gmail.com>
2023-08-25 23:16:14 +0100

    urls update

--- weather/weatherApp/urls.py
+++ weather/weatherApp/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.Home, name='home')
+    path('', views.Home, name='home'),
+    path('about/',views.about,name="about"),
+    path('help/',views.help,name="help"),
 ]
